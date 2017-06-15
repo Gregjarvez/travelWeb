274538b82f1dcf8244b9a179be519eff56d5fb42
GREGORY ASSASIE <gregjarvez@gmail.com>
2017-06-15 03:47:09 +0100

    session complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,1 @@
-require("./gulp/tasks/sassfile");require("./gulp/tasks/serverfile");require("./gulp/tasks/watch");require("./gulp/tasks/sprite");require("./gulp/tasks/scripts");
+require("./gulp/tasks/sassfile");require("./gulp/tasks/serverfile");require("./gulp/tasks/watch");require("./gulp/tasks/sprite");require("./gulp/tasks/scripts");require("./gulp/tasks/build");
